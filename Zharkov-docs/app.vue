27d84f5b554be2240d918019d5bc21330b6ecef0
Vercel <vercel[bot]@users.noreply.github.com>
2026-02-27 01:52:47 +0000

    Integrate Vercel Web Analytics into Nuxt
    This is an automated pull request generated by Vercel to help install [Web Analytics](https://vercel.com/docs/analytics) in your application.
    
    Co-authored-by: Vercel <vercel[bot]@users.noreply.github.com>

---
+++ Zharkov-docs/app.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Analytics } from '@vercel/analytics/nuxt';
+</script>
+
+<template>
+  <div>
+    <Analytics />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
