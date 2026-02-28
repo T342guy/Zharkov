6567164fab0c97e06d8b5ca5f1ced09510a605f3
T342 <chaircomputeroverdrive@gmail.com>
2026-02-28 12:37:50 -0800

    edit nuxt config, and the footer DOES WORK, plus edit the desc.

--- Zharkov-docs/components/AppFooterLeft.vue
+++ Zharkov-docs/components/AppFooterLeft.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="text-sm text-muted">
-    Copyright © T342guy 2026. All content is licensed under the CC BY-SA 4.0 license unless otherwise noted.
+    Copyright © T342guy 2026 <br/>
+    All content is licensed under the CC BY-SA 4.0 license unless otherwise noted.
   </div>
 </template>
